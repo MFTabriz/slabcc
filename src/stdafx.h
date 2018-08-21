704361a3867ed08df790f270fabfd948f494637e
mtabriz <mtabriz@pm.me>
2018-08-21 16:19:17 +0200

    comment ARMA_NO_DEBUG flag
    this is overrided by makefile

--- src/stdafx.h
+++ src/stdafx.h
@@ -4,8 +4,8 @@
 
 #pragma once
 
-//comment the next line while developing the code! Uncommenting will disable the range checks in Armadillo.
-#define ARMA_NO_DEBUG
+//comment the next line while developing the code! Uncommenting will disable the range checks in Armadillo. The -DARMA_NO_DEBUG flag in makefile may override this
+//#define ARMA_NO_DEBUG
 
 
 #include "targetver.h"
@@ -13,16 +13,16 @@
 #include <future>
 
 #include <stdio.h>
-#include <iomanip> 
-#include <iostream> 
-#include <fstream> 
+#include <iomanip>
+#include <iostream>
+#include <fstream>
 #include <sstream>
 
 #include <chrono>
-#include <vector>  
+#include <vector>
 #include <unordered_map>
-#include <string>  
+#include <string>
 
-#include <algorithm> 
+#include <algorithm>
 
 
