b1c1c52d0ec7d979848d4093a5b036195f737b64
mtabriz <mtabriz@pm.me>
2018-10-10 20:06:08 +0200

    fix the digits' count

--- src/slabcc_math.cpp
+++ src/slabcc_math.cpp
@@ -209,7 +209,10 @@ void write_mat2file(const mat& input, const string& output_file) {
 	ofstream out_file;
 	out_file.open(output_file);
 	out_file << fixed << showpos << setprecision(15);
-	input.each_row([&out_file](const rowvec &row) { out_file << row << endl; });
+	input.each_row([&out_file](const rowvec &row) { 
+		row.for_each([&out_file](const double& val) { out_file << val << " "; });
+		out_file << endl; 
+	});
 	out_file.close();
 }
 
